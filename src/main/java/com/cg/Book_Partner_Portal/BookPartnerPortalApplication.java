fdb3347e51f5e5ef61f3fb126f1aea7d17f2980c
Sid489 <sid068159@gmail.com>
2026-03-25 12:40:47 +0530

    Initial Commit

---
+++ src/main/java/com/cg/Book_Partner_Portal/BookPartnerPortalApplication.java
@@ -0,0 +1,13 @@
+package com.cg.Book_Partner_Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPartnerPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookPartnerPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/Book_Partner_Portal/BookPartnerPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.Book_Partner_Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPartnerPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
