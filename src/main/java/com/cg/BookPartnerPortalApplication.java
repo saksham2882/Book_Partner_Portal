8bc39ca6371216c80b9b96bdbf089c5368d03752
Saksham Agrahari <agrahari0899@gmail.com>
2026-03-27 10:27:07 +0530

    check branch

---
+++ src/main/java/com/cg/BookPartnerPortalApplication.java
@@ -0,0 +1,14 @@
+package com.cg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookPartnerPortalApplication {
+
+	// main
+	public static void main(String[] args) {
+		SpringApplication.run(BookPartnerPortalApplication.class, args);
+	}
+
+}
