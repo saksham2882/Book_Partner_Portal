b484088a3d446c49d22bbf8817e457eff787a721
Deependra Verma <ddeependra2004@gmail.com>
2026-03-30 12:44:08 +0530

    removing unused code AuthorBookPublisherDto

--- src/main/java/com/cg/service/IAuthorService.java
+++ src/main/java/com/cg/service/IAuthorService.java
@@ -1,7 +1,6 @@
 package com.cg.service;
 
 import com.cg.dto.AuthorBookPublisherDTO;
-import com.cg.dto.AuthorBookPublisherDto;
 import com.cg.dto.AuthorRoyaltyDTO;
 import com.cg.dto.BestSellingBookDTO;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +9,6 @@ import java.util.List;
 
 public interface IAuthorService {
     List<AuthorBookPublisherDTO> getAuthorsWithBooksAndPublishers();
-    List<AuthorBookPublisherDto> getAuthorsWithBooksAndPublishers();
 
     List<BestSellingBookDTO> getBestSellingBooks();
 
